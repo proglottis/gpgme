9e9c74f24317e12fe7d7680c2ed631d2af8be48f
James Fargher <proglottis@gmail.com>
2015-12-20 13:43:18 +1300

    Fix cgo callbacks for go 1.6

--- gpgme_test.go
+++ gpgme_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"flag"
 	"io"
-	"io/ioutil"
 	"os"
 	"runtime"
 	"strings"
@@ -129,74 +128,6 @@ func TestContext_DecryptVerify(t *testing.T) {
 	checkError(t, err)
 	checkError(t, ctx.DecryptVerify(cipher, plain))
 	diff(t, buf.Bytes(), []byte("Test message\n"))
-}
-
-func TestNewData(t *testing.T) {
-	dh, err := NewData()
-	checkError(t, err)
-	for i := 0; i < 5; i++ {
-		_, err := dh.Write([]byte(testData))
-		checkError(t, err)
-	}
-	_, err = dh.Seek(0, 0)
-	checkError(t, err)
-
-	var buf bytes.Buffer
-	_, err = io.Copy(&buf, dh)
-	checkError(t, err)
-	expected := bytes.Repeat([]byte(testData), 5)
-	diff(t, buf.Bytes(), expected)
-
-	dh.Close()
-}
-
-func TestDataNewDataFile(t *testing.T) {
-	f, err := ioutil.TempFile("", "gpgme")
-	checkError(t, err)
-	defer func() {
-		f.Close()
-		os.Remove(f.Name())
-	}()
-	dh, err := NewDataFile(f)
-	checkError(t, err)
-	defer dh.Close()
-	for i := 0; i < 5; i++ {
-		_, err := dh.Write([]byte(testData))
-		checkError(t, err)
-	}
-	_, err = dh.Seek(0, 0)
-	checkError(t, err)
-	var buf bytes.Buffer
-	_, err = io.Copy(&buf, dh)
-	checkError(t, err)
-	expected := bytes.Repeat([]byte(testData), 5)
-	diff(t, buf.Bytes(), expected)
-}
-
-func TestDataNewDataReader(t *testing.T) {
-	r := bytes.NewReader([]byte(testData))
-	dh, err := NewDataReader(r)
-	checkError(t, err)
-	var buf bytes.Buffer
-	_, err = io.Copy(&buf, dh)
-	checkError(t, err)
-	diff(t, buf.Bytes(), []byte(testData))
-
-	dh.Close()
-}
-
-func TestDataNewDataWriter(t *testing.T) {
-	var buf bytes.Buffer
-	dh, err := NewDataWriter(&buf)
-	checkError(t, err)
-	for i := 0; i < 5; i++ {
-		_, err := dh.Write([]byte(testData))
-		checkError(t, err)
-	}
-	expected := bytes.Repeat([]byte(testData), 5)
-	diff(t, buf.Bytes(), expected)
-
-	dh.Close()
 }
 
 func skipGPG2x(t *testing.T, msg string) {
